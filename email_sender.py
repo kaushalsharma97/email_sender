9978321654acab8753a98ecf3ec9df7c1d71748c
Kaushal <sharmakaushal.er@gmail.com>
2021-01-25 13:54:16 +0530

    Project Updated

---
+++ email_sender.py
@@ -0,0 +1,21 @@
+import smtplib
+from email.message import EmailMessage
+from string import Template
+from pathlib import Path
+
+html = Template(Path('index.html').read_text())
+email = EmailMessage()
+email['from'] = 'Kaushal Sharma'
+email['to'] = 'sharmakaushal.er@gmail.com'
+email['subject'] = 'YOU HAVE WON 100000 DOLLARS!'
+
+email.set_content(html.substitute({'name': 'Tintin'}),'html')
+
+with smtplib.SMTP(host='smtp.gmail.com',port= 587) as smtp:
+	smtp.ehlo()
+	smtp.starttls()
+	smtp.login('sharmakaushal.er@gmail.com','Kaushal@123')
+	smtp.send_message(email)
+	print('all good boss!')
+
+
